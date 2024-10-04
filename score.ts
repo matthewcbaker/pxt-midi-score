da2c8984fad16b18bbde55108744c144a1199cdf
Matt Baker <matt@matt-baker.org>
2024-10-04 18:04:46 +0100

    Initial files for MakeCode project

---
+++ score.ts
@@ -0,0 +1,16 @@
+
+/**
+ * Musical Score
+ */
+//% weight=84 icon="\uf039" color="#5ea9dd"
+namespace score {
+    //% block="score key"
+    export function setKey() {
+
+    }
+
+    //% block="note name $name"
+    export function noteName(name: string): number {
+        return 60
+    }
+}
